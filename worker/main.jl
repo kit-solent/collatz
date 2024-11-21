cb5951a686430d8aca9b3026b1d53ebb20b6a5a0
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-11-21 20:37:06 +1300

    SaveBot commit at 2024-11-21 20:37:06

--- worker/main.jl
+++ worker/main.jl
@@ -1,3 +1,51 @@
+#### Collatz Conjecture testing program
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+
+## ./data
+# the data file is in the following format:
+# worker_id,starting_value,goal,current_value (optional)
+
+# worker_id is the id that has been asigned to this worker.
+# it is used when the assigned range has been fully computed
+# to report success.
+
+# starting_value is the value from which computations start.
+# it is used as a fallback if current_value is not there.
+
+# goal is the number up to which computation will occour.
+# when this number is reached we can report success.
+
+# current_value is our current progress in our computations.
+# when chunks are computed current_value is incremented to
+# reflect the numbers we have scanned. If not present
+# starting_value can be used as a fallback.
+
+# below is the mathematical reasoning behind the 4n + 3 search space.
+#
+# 2n -> n and n < 2n so discard 2n leaving 2n + 1
+# split 2n + 1 into 4n + 1, 4n + 3
+# 4n + 1 (odd) -> 1.5(4n + 1) + 0.5 = 6n + 2 = 3n + 1 and 3n + 1 < 4n + 1 so discard 4n + 1 leaving 4n + 3
+# 4n + 3 can be split but cannot be narrowed further.
+
+# a) if the number enters a loop that excludes 1
+# b) if the number grows without bounds and tends to infinity.
+# In either of these cases then the test function will block forever (intended behavior)
+
+# If a number ever falls below it's starting value it can be discarded. There are two ways of justifying this.
+#
+# a) since we are testing numbers starting low and going up, if a number ever drops below it's starting value
+# then it must now be at a number we have already tested and found to be "normal" (does not break the conjecture)
+#
+# b) lets say that we are only searching for the lowest counterexample to the conjecture, then if a counterexample falls
+# below it's starting value then we have found another lower counterexample so the first could not have been the lowest.
+# this is because if n is a counterexample then EVERY number in it's sequence also must be a counterexample coz common sense.
+
+
+# TODO: Benchmark out-of-loop precomputation of the first two 1.5n+1 steps and passing in both values vs computing in function.
+
+# TODO: Try putting all the below into a main function and benchmark.
+
+
 
 #region tools
 """Functions to create, steralise, load, save and compute various test ranges."""
@@ -11,6 +59,10 @@ using Nemo, Base.Threads
 # at a time. If the program is terminated the progress will fall back
 # on the last completed chunk.
 const CHUNK_SIZE::ZZRingElem = ZZ(10)^7
+
+# this is the number of cases that will be computed in a given pass of test_range
+# used to allow exclusion of ranges based on the computations of form.jl
+const RANGE_UNROLL_COUNT::ZZRingElem = ZZ(256)
 
 # This regex reperesents 4 unsigned integers seperated by commas e.g. 1234,65324,2,2345 or 1,0,0,5364
 const VALID_FILE_REGEX::Regex = r"^[0-9]+(,[0-9]+){2,3}$"
@@ -126,7 +178,7 @@ end
 end
 
 """Generate and return `threads` number of valid search ranges that ensure that `range` is fully covered."""
-function generate_ranges(range::StepRange{ZZRingElem}, threads::Int)::Tuple{Vararg{StepRange{ZZRingElem}}}
+@inline function generate_ranges(range::StepRange{ZZRingElem}, threads::Int)::Tuple{Vararg{StepRange{ZZRingElem}}}
     # formula to find closest number of form 4n + 3: 3, 7, 11, 15, 19, 23, 27
     # n-(n-3)%4 is rounded down
     # n+(3-n%4)%4 is rounded up
@@ -140,7 +192,7 @@ function generate_ranges(range::StepRange{ZZRingElem}, threads::Int)::Tuple{Vara
 end
 
 """Splits the given range into chunks of size `chunk_size`."""
-function generate_chunks(range::StepRange{ZZRingElem}, chunk_size::ZZRingElem = CHUNK_SIZE)::Tuple{Vararg{StepRange{ZZRingElem}}}
+@inline function generate_chunks(range::StepRange{ZZRingElem}, chunk_size::ZZRingElem = CHUNK_SIZE)::Tuple{Vararg{StepRange{ZZRingElem}}}
     # return the ntuple
     ntuple(
         # This is the calculation for the ith item.
@@ -173,6 +225,12 @@ function test_range_skip_4(range::StepRange{ZZRingElem})::Nothing
     end
 end
 
+
+function test_range_unrolled(range::StepRange{ZZRingElem})::Nothing
+    # here range is expected to be valid search numbers.
+end
+
+
 """Tests the given chunk using the @threads macro and test_range."""
 function test_chunk(chunk::StepRange{ZZRingElem})::Nothing
     # create ranges from the chunk
@@ -187,89 +245,6 @@ end
 end
 #endregion
 
-#### Collatz Conjecture testing program
-# takes about 5 minutes and 5.71 seconds to compute 10^10 numbers.
-#
-# the data file is in the following format:
-# worker_id,starting_value,goal,current_value (optional)
-
-# worker_id is the id that has been asigned to this worker.
-# it is used when the assigned range has been fully computed
-# to report success.
-
-# starting_value is the value from which computations start.
-# it is used as a fallback if current_value is not there.
-
-# goal is the number up to which computation will occour.
-# when this number is reached we can report success.
-
-# current_value is our current progress in our computations.
-# when chunks are computed current_value is incremented to
-# reflect the numbers we have scanned. If not present
-# starting_value can be used as a fallback.
-
-
-
-# below is the mathematical reasoning behind my only testing numbers of the form 4n + 3
-
-# https://en.wikipedia.org/wiki/Collatz_conjecture
-
-# README
-# If you can't be bothered reading the wikipedia page here are the basics.
-# the conjecture deffines a process when if a number n is even it is halved
-# and if it is odd then 3n+1 is applied. It conjectures that EVERY positive
-# integer will, with enough iterations of this process eventually reach 1
-# where it will fall into the cycle: 1, 4, 2, 1
-# (1 is odd so gets 3n + 1 to become 4 which is even and so gets halved to 2 and again to 1 again)
-# There are two ways that a number could break this conjecture.
-# a) if the number enters a loop that excludes 1
-# b) if the number grows without bounds and tends to infinity.
-# In either of these cases then the test(number::ZZRingElem) function will block forever
-# this is not a problem though as most professional mathematicians consider this conjecture
-# true and so we are really just proving what is probably true.
-# The conjecture is still unproven however and so if you do perchance find a number that
-# breaks it then you are instantly the worlds most famous mathematician of the 21st centuary.
-
-# If a number ever falls below it's starting value it can be discarded. There are two ways of justifying this.
-# a) since we are testing numbers starting low and going up, if a number ever drops below it's starting value
-# then it must now be at a number we have already tested and found to be "normal" (does not break the conjecture)
-
-# b) lets say that we are only searching for the lowest counterexample to the conjecture, then if a counterexample falls
-# below it's starting value then we have found another lower counterexample so the first could not have been the lowest.
-# this is because if n is a counterexample then EVERY number in it's sequence also must be a counterexample coz common sense.
-
-# search space = all positive integers
-# even numbers are imediatly halved (fall below starting value) so:
-# search space = 2n + 1 (odd numbers)
-
-# every second odd number (4n + 1) becomes:
-# 3(4n + 1) + 1 = 12n + 4  is even so...
-# 6n + 2                   still even so...
-# 3n + 1
-# 3n + 1 < 4n + 1 for n in positive integers so every 2nd odd number will certainly drop below it's starting value.
-
-# search space = 4n + 3
-#          *          *           *              * = can be excluded from the search.
-# 4n + 3:  3  7 11 15 19 23 27 31 35 39 ...      these are all odd so 1.5(4n + 3) + 0.5 = 6n + 5
-# 6n + 5:  5 11 17 23 29 35 41 47 53 59 ...          still all odd so 1.5(6n + 5) + 0.5 = 9n + 8
-# 9n + 8:  8 17 26 35 44 53 62 71 80 89 ...      every 2nd of which is even
-#       :  4    13    22    31    40    ...      every 2nd of which is even again
-#       :  2          11          20             which are all smaller. These are every 4th 4n + 3 number or 16n + 3
-
-# So every 4th number of the form 4n + 3 can be discarded. # TODO: test 16n + 7, 16n + 11, 16n + 15
-
-# every 4th term is 16n + 3 = 2(8n + 2) + 1 is odd so
-# 1.5(16n + 3) + 0.5 = 24n + 5 = 2(12n + 2) + 1 is odd so
-# 1.5(24n + 5) + 0.5 = 36n + 8 = 2(18n + 4) is even so
-# 18n + 4                      = 2( 9n + 2) is even so
-#  9n + 2 and 9n + 2 < 16n + 3 so can be discarded.
-
-# Given that all numbers passed into test(number) are odd twice i.e. they are odd and so get 1.5n + 0.5 applied
-# remain odd and so get it applied again, some steps could be precomputed to some degree.
-
-# TODO: Benchmark out-of-loop precomputation of the first two 1.5n+1 steps and passing in both values vs computing in function.
-
-# TODO: Try putting all the below into a main function and benchmark.
 
 using .Tools
 
