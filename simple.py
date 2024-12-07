fbe6effd2666671a39787aa2274c74ffbc2ff7e6
nelsonmaxlang@gmail.com <nelsonmaxlang@gmail.com>
2024-12-07 21:20:59 +1300

    SaveBot commit at 2024-12-07 21:20:59

--- simple.py
+++ simple.py
@@ -14,6 +14,7 @@ def will_fall(a:f, b:f):
         data[1] = [x, y]
         if x % 2 == 0:
             if y % 2 == 0:
+                # if both x and y are even then xn + y is even so halve both of them.
                 x /= 2
                 data[2][0] /= 2
                 y /= 2
@@ -22,10 +23,12 @@ def will_fall(a:f, b:f):
                 if x < a or (x == a and y < b):
                     return (True, data)
             else:
+                # if x is even and y is odd then xn + y is odd so apply (3n + 1)/2
+                # (3(xn + y) + 1)/2 = (3x/2)n + (3y + 1)/2
                 x = (3*x) / 2
                 data[2][0] = (3*data[2][0]) / 2
                 y = (3*y + 1) / 2
-                data[2][1] = (3*data[2][0] + 1) / 2
+                data[2][1] = (3*data[2][1] + 1) / 2
         else:
             return (False, data)
 
@@ -41,6 +44,9 @@ def nice_fraction(fraction:f):
 for i in range(256):
     x = will_fall(256, f(i))
     if not x[0]:
-        print(f"{x[1][0][0]}n + {x[1][0][1]}", end="\t\t")
-        print(f"{nice_fraction(x[1][1][0])}n + {nice_fraction(x[1][1][1])}", end="\t\t")
-        print(f"{nice_fraction(x[1][2][0])}n + {nice_fraction(x[1][2][1])}", end="\t\t\n")
+        print(f"{x[1][0][0]}n + {x[1][0][1]}", end="\t")
+        print(f"{nice_fraction(x[1][1][0])}n + {nice_fraction(x[1][1][1])}", end="\t")
+        print(f"({nice_fraction(x[1][2][0])})n + {nice_fraction(x[1][2][1])}", end="\t")
+        if x[1][2][0].denominator == x[1][2][1].denominator:
+            # if the denominators are the same then an expression can be formed that avoids floating point calculations.
+            print(f"({x[1][2][0].numerator}*(i + {x[1][0][1]}) + {x[1][2][1].numerator}) / {x[1][2][0].denominator}", end="\n")
